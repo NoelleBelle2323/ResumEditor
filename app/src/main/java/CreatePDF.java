a465766906b75238b600d93f847853823721a91c
NoelleBelle23 <DogWhisperer125@comcast.net>
2017-04-11 19:04:58 -0700

    Arrow changes/fixed null pop-up/started new resume screen process

---
+++ app/src/main/java/CreatePDF.java
@@ -0,0 +1,14 @@
+import com.itextpdf.text.Document;
+
+/**
+ * Created by Noelle on 4/5/2017.
+ */
+
+public class CreatePDF {
+
+    public CreatePDF(Document doc){
+
+    }
+
+
+}
---
+++ app/src/main/java/PDFText.java
@@ -0,0 +1,61 @@
+/**
+ * Created by Noelle on 4/4/2017.
+ */
+
+public class PDFText {
+    public static String name;
+    public static String address;
+    public static String email;
+    public static String phoneNum;
+
+    public static String university;
+    public static String gradYear;
+    public static String degree;
+    public static String schoolLoc;
+
+    public static String compName;
+    public static String position;
+    public static String workYears;
+    public static String responsibilities;
+
+    public PDFText(){
+        name = "";
+        address = "";
+        email = "";
+        phoneNum = "";
+        university = "";
+        gradYear = "";
+        degree = "";
+        schoolLoc = "";
+        compName = "";
+        position = "";
+        workYears = "";
+        responsibilities = "";
+
+    }
+
+    public void setName(String n) {name = n;}
+
+    public void setAddress(String a) {address = a;}
+
+    public void setEmail(String e) {email = e;}
+
+    public void setPhoneNum(String num) {phoneNum = num;}
+
+    public void setUniversity(String u) {university = u;}
+
+    public void setGradYear(String y) {gradYear = y;}
+
+    public void setDegree(String d) {degree = d;}
+
+    public void setSchoolLoc(String loc) {schoolLoc = loc;}
+
+    public void setCompName(String name) {compName = name;}
+
+    public void setPosition(String pos) {position = pos;}
+
+    public void setWorkYears(String yrs) {workYears = yrs;}
+
+    public void setResponsibilities(String r) {responsibilities = r;}
+
+}
